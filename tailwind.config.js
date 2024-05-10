c9aff0e8b5f4c4da93308441209a1105f7f11d80
Piotr Zalejski <piotrzalejski@outlook.com>
2024-05-09 23:14:32 -0500

    update styling for blog template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./public/**/*.{html,js}', './.elevemty.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"M PLUS Rounded 1c"', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'bg-dark': '#0d0f14',
+        'text-dark': '#f2f6fa',
+      },
+    },
   },
   plugins: [],
 };
